1961cea73eafa7eefad6d6bd31683f55c46a6150
吕永博 <bengbengle@gmail.com>
2022-03-06 21:06:16 +0800

    zh-cn

--- uniswap-v2-interface/src/hooks/useLang.ts
+++ uniswap-v2-interface/src/hooks/useLang.ts
@@ -2,7 +2,7 @@ import useParsedQueryString from './useParsedQueryString'
 
 export enum I18 {
   opt1 = 'en',
-  opt2 = 'zh-CN'
+  opt2 = 'zh-TW'
 }
 
 export const default_Lang: I18 = I18.opt1
@@ -10,6 +10,6 @@ export const default_Lang: I18 = I18.opt1
 export default function useLang(): I18 {
   const { i18 } = useParsedQueryString()
   if (!i18 || typeof i18 !== 'string') return I18.opt1
-  if (i18.toLowerCase() === 'zh-CN') return I18.opt2
+  if (i18.toLowerCase() === 'zh-TW') return I18.opt2
   return default_Lang
 }
